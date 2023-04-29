17910952dc0ac15f9d80f20e60adb12984464e04
moe10291 <mohammad57zero@gmail.com>
2023-04-29 17:33:36 -0400

    Routes and Pages added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Components/Home';
+import SignIn from './Components/SignIn';
+import Signup from './Components/Signup';
+import ForgotPassword from './Components/ForgotPassword';
+import Profile from './Components/Profile';
+import Offers from './Components/Offers';
 function App() {
   return (
     <div className="App">
-<h1 className="text-5xl">
-      Hello world!
-    </h1>
+          <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/sign-up' element={<Signup/>} />
+      <Route path='/forgot-password' element={<ForgotPassword/>} />
+      <Route path='/profile' element={<Profile/>} />
+      <Route path='/offers' element={<Offers/>} />
+    </Routes>
+</BrowserRouter>
     </div>
   );
 }
