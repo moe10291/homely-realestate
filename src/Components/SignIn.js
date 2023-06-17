c62b388d07977c1b08c205c06c8305fb423c7bef
moe10291 <mohammad57zero@gmail.com>
2023-06-17 10:09:11 -0400

    sign in created

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { useState } from 'react';
 import key from '../dragonfly-ave-pTZJTasRzY0-unsplash.jpg';
 import {AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import OAuth from './OAuth';
+import { signInWithEmailAndPassword, getAuth} from 'firebase/auth';
+import {toast} from "react-toastify";
+
 
 export default function SignIn() {
   const [showPassword, setShowPassword]= useState(false);
@@ -13,11 +16,25 @@ export default function SignIn() {
   });
 
   const {email, password}= formData;
+  const navigate= useNavigate();
   function onChange(e){
     setFormData((prevState)=>({
       ...prevState,
       [e.target.id]: e.target.value
     }))
+  }
+
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth= getAuth()
+      const userCredntial= await signInWithEmailAndPassword(auth, email, password)
+      if(userCredntial.user){
+        navigate("/")
+      }
+    } catch (error) {
+      toast.error("The Username/Password is incorrect")
+    }
   }
   return (
     <div>
@@ -34,7 +51,7 @@ export default function SignIn() {
     </div>
 
     <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-      <form>
+      <form onSubmit={onSubmit}>
         <input 
         className='w-full px-4 py-2 text-md border-black rounded transition ease-linear mb-6' 
         type="email" 
